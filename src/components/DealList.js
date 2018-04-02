40d3c43f37a9a284319b6ecd64514f5034e33353
Yeshu Kochher <yeshu@outlook.in>
2018-04-02 18:40:24 +0530

    showing names in app

--- src/components/DealList.js
+++ src/components/DealList.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-class DealList extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Deals</Text>
-      </View>
-    );
-  }
-}
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#eee',
+    flex: 1,
+    width: '100%',
+  },
+});
+
+const DealList = ({ deals }) => (
+  <View style={styles.list}>
+    <FlatList
+      data={deals}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+    />
+  </View>
+);
+
+DealList.propTypes = {
+  deals: PropTypes.array.isRequired,
+};
 
 export default DealList;
